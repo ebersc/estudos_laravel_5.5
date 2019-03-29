8b85bb3381425ca0f32159a0c60abc37174add2e
Eberson Dos Santos Cosme <eberson.cosme@ideal-823.idealinvest.local>
2019-03-29 15:19:39 -0300

    Criação dos arquivos para cadastro de saldo

--- laravel55-saldo/routes/web.php
+++ laravel55-saldo/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-Route::group(['middleware' => ['auth'], 'namespace' => 'Admin'], function () {
-    Route::get('admin', 'AdminController@index')->name('admin');    
+Route::group(['middleware' => ['auth'], 'namespace' => 'Admin', 'prefix' => 'admin'], function () {
+    Route::get('balance', 'BalanceController@index')->name('admin.balance');
+    
+    Route::get('/', 'AdminController@index')->name('admin');    
 });
 
 Route::get('/', 'Site\SiteController@index')->name('home');
