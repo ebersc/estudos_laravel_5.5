342471fbc4e0eae83f9105a5150498c397734e62
Eberson Dos Santos Cosme <eberson.cosme@ideal-823.idealinvest.local>
2019-04-02 09:36:22 -0300

    Criação da tela de recarga

--- laravel55-saldo/routes/web.php
+++ laravel55-saldo/routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 Route::group(['middleware' => ['auth'], 'namespace' => 'Admin', 'prefix' => 'admin'], function () {
+    
+    Route::get('balance/deposit', 'BalanceController@deposit')->name('balance.deposit');
+
+    Route::post('balance/deposit', 'BalanceController@depositStore')->name('deposit.store');
+
     Route::get('balance', 'BalanceController@index')->name('admin.balance');
     
     Route::get('/', 'AdminController@index')->name('admin');    
